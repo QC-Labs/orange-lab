b2bcb5697529817f1a43335b3570b3d8495a74f8
Adam Nowotny <adam.nowotny@protonmail.com>
2025-01-01 15:51:36 +0100

    refactor: move AI components to AIModule

---
+++ components/ai-module.ts
@@ -0,0 +1,63 @@
+import * as pulumi from '@pulumi/pulumi';
+import { KubeAi } from './ai/kubeai';
+import { Ollama } from './ai/ollama';
+import { OpenWebUI } from './ai/open-webui';
+
+interface AIModuleArgs {
+    domainName: string;
+    gpuStorageClass: string;
+}
+
+export class AIModule extends pulumi.ComponentResource {
+    ollamaUrl: string | undefined;
+    kubeAIUrl: string | undefined;
+    openWebUIUrl: string | undefined;
+
+    private config = new pulumi.Config('orangelab');
+    private ollama: Ollama | undefined;
+    private kubeAI: KubeAi | undefined;
+    private openWebUI: OpenWebUI | undefined;
+
+    constructor(
+        name: string,
+        args: AIModuleArgs,
+        opts?: pulumi.ComponentResourceOptions,
+    ) {
+        super('ai', name, args, opts);
+
+        if (this.isModuleEnabled('ollama')) {
+            this.ollama = new Ollama('ollama', {
+                domainName: args.domainName,
+                storageClass: args.gpuStorageClass,
+            });
+            this.ollamaUrl = this.ollama.endpointUrl;
+        }
+
+        if (this.isModuleEnabled('kubeai')) {
+            this.kubeAI = new KubeAi('kubeai', {
+                domainName: args.domainName,
+            });
+            this.kubeAIUrl = this.kubeAI.serviceUrl;
+        }
+
+        if (this.isModuleEnabled('open-webui')) {
+            this.openWebUI = new OpenWebUI(
+                'open-webui',
+                {
+                    domainName: args.domainName,
+                    storageClass: args.gpuStorageClass,
+                    ollamaUrl: this.ollama?.serviceUrl,
+                    openAiUrl: this.kubeAI?.serviceUrl,
+                },
+                {
+                    dependsOn: [this.ollama, this.kubeAI].filter(x => x !== undefined),
+                },
+            );
+            this.openWebUIUrl = this.openWebUI.endpointUrl;
+        }
+    }
+
+    public isModuleEnabled(name: string): boolean {
+        return this.config.requireBoolean(name);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
 import * as pulumi from '@pulumi/pulumi';
-import { KubeAi } from './components/ai/kubeai';
-import { Ollama } from './components/ai/ollama';
-import { OpenWebUI } from './components/ai/open-webui';
+import { AIModule } from './components/ai-module';
 import { HomeAssistant } from './components/iot/home-assistant';
 import { SystemModule } from './components/system-module';
 
-export const system = new SystemModule('system');
+const system = new SystemModule('system');
 export const tailscaleServerKey = system.tailscaleServerKey;
 export const tailscaleAgentKey = system.tailscaleAgentKey;
 export const tailscaleDomain = system.domainName;
@@ -34,48 +32,14 @@ if (system.isModuleEnabled('home-assistant')) {
 }
 export const iotHomeAssistantUrl = homeAssistant?.endpointUrl;
 
-/**
- * AI
- */
-
-let kubeAI;
-if (system.isModuleEnabled('kubeai')) {
-    kubeAI = new KubeAi(
-        'kubeai',
-        {
-            domainName: system.domainName,
-        },
-        { dependsOn: [system] },
-    );
-}
-export const aiKubeAIUrl = kubeAI?.endpointUrl;
-export const aiKubeAIInternalUrl = kubeAI?.serviceUrl;
-
-let ollama;
-if (system.isModuleEnabled('ollama')) {
-    ollama = new Ollama(
-        'ollama',
-        {
-            domainName: system.domainName,
-            storageClass: system.gpuStorageClass,
-        },
-        { dependsOn: [system] },
-    );
-}
-export const aiOllamaUrl = ollama?.endpointUrl;
-export const aiOllamaInternalUrl = ollama?.serviceUrl;
-
-let openWebUI;
-if (system.isModuleEnabled('open-webui') && ollama) {
-    openWebUI = new OpenWebUI(
-        'open-webui',
-        {
-            domainName: system.domainName,
-            ollamaUrl: ollama.serviceUrl,
-            openAiUrl: kubeAI?.serviceUrl,
-            storageClass: system.gpuStorageClass,
-        },
-        { dependsOn: [ollama, system] },
-    );
-}
-export const aiOpenWebUIUrl = openWebUI?.endpointUrl;
+const aiModule = new AIModule(
+    'ai',
+    {
+        domainName: system.domainName,
+        gpuStorageClass: system.gpuStorageClass,
+    },
+    { dependsOn: [system] },
+);
+export const aiOllamaUrl = aiModule.ollamaUrl;
+export const aiKubeAIUrl = aiModule.kubeAIUrl;
+export const aiOpenWebUIUrl = aiModule.openWebUIUrl;
