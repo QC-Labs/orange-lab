b87e350aa26ee12d2dc11263f9c890ec9bb079da
Adam Nowotny <adam@orangelab.space>
2026-01-11 15:57:31 +0930

    bitcoin: add --help to bitcoin-cli if no params specified

--- scripts/bitcoin-cli.sh
+++ scripts/bitcoin-cli.sh
@@ -4,4 +4,4 @@
 
 ADMINPASSWORD=$(pulumi stack output bitcoin --show-secrets | jq .bitcoinUsers.admin -r)
 
-bitcoin-cli -rpcconnect=bitcoin -rpcuser=admin -rpcpassword="${ADMINPASSWORD}" "$@"
+bitcoin-cli -rpcconnect=bitcoin -rpcuser=admin -rpcpassword="${ADMINPASSWORD}" "${@:---help}"
