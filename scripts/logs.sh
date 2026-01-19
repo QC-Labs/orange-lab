3e47a6022e4dca53b3bfe11f42a90208b65d124e
Adam Nowotny <adam@orangelab.space>
2026-01-19 20:25:06 +1000

    logs: retry every 10 seconds when logs script exits

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -3,6 +3,8 @@
 # Script to quickly access application logs
 # Usage: logs <app-name> [namespace]
 
+trap "exit" INT TERM
+
 appName=$1
 namespace=${2:-$1}  # app name as namespace if not provided
 label=app.kubernetes.io/name
@@ -17,4 +19,8 @@ if [[ "$appName" == "open-webui" ]]; then
     label=app.kubernetes.io/component
 fi
 
-kubectl logs -f -l $label=$appName -n $namespace --all-containers=true --ignore-errors=true --pod-running-timeout=5m
+while true; do
+  kubectl logs -f -l $label=$appName -n $namespace --all-containers=true --ignore-errors=true --pod-running-timeout=5m
+  echo "*** Connection lost *** Retrying in 10 seconds..."
+  sleep 10
+done
