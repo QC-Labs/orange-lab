561001df40ff282ca78e990a57f0f9d256f145e4
Adam Nowotny <adam@orangelab.space>
2026-05-19 00:48:43 +0200

    fix(storage): reference config volumes by full name

--- packages/pulumi/src/storage.ts
+++ packages/pulumi/src/storage.ts
@@ -201,7 +201,7 @@ export class Storage extends pulumi.ComponentResource {
         );
         this.volumes.set(name, {
             name,
-            configMap: { name },
+            configMap: { name: fullName },
         });
     }
 
